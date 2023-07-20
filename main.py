4e4a50a6e135e527c80d417d534ea7f623af7a2d
Edvinas Afarjancas <eafarjancas@gmail.com>
2023-07-20 22:02:39 +0300

    second phase

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import logging
 from typing import List
-from utilities.database import cpus_list
-from utilities.pc_parts import CPU
+from utilities.crud_operations import read_cpu, update_cpu, delete_cpu
+from utilities.database import cpus_list, cpu_collers_list, motherboards_list, memories_list, storages_list, video_cards_list, cases_list, power_supplies_list
+from utilities.pc_parts import CPU, CPUColler, Motherboard, Memory, Storage, VideoCard, Case, PowerSupply
+
+logging.basicConfig(level=logging.DEBUG, filename="data_flow.log", filemode="w",
+                    format="%(asctime)s - %(levelname)s - %(message)s")
 
 
 class PartPicker:
@@ -8,25 +13,46 @@ class PartPicker:
         self._cpus = cpus
 
     def get_processors_by_brand(self, brand: str) -> List[CPU]:
-        processors_list = []
-        for cpu in self._cpus:
-            if cpu.get_brand() == brand:
-                processors_list.append(cpu)
-        return processors_list
+        try:
+            logging.debug(f"Getting processors by brand: {brand}")
+            processors_list = []
+            for cpu in self._cpus:
+                if cpu.get_brand() == brand:
+                    processors_list.append(cpu)
+            return processors_list
+        except Exception as e:
+            logging.error(f"Error in get_processors_by_brand: {str(e)}")
+            return e
 
     def get_cpus_by_cores(self, core_count: int) -> List[CPU]:
-        processors_list = []
-        for cpu in self._cpus:
-            if cpu.get_core_count() == core_count:
-                processors_list.append(cpu)
-        return processors_list
+        try:
+            logging.debug(f"Getting CPUs by core count: {core_count}")
+            processors_list = []
+            for cpu in self._cpus:
+                if cpu.get_core_count() == core_count:
+                    processors_list.append(cpu)
+            return processors_list
+        except Exception as e:
+            logging.error(f"Error in get_cpus_by_cores: {str(e)}")
+            return e
 
     def get_cpus_by_price(self, price: float) -> List[CPU]:
-        processors_list = []
-        for cpu in self._cpus:
-            if cpu.get_price() <= price:
-                processors_list.append(cpu)
-        return processors_list
+        try:
+            logging.debug(f"Getting CPUs by price: {price}")
+            processors_list = []
+            for cpu in self._cpus:
+                if cpu.get_price() <= price:
+                    processors_list.append(cpu)
+            return processors_list
+        except Exception as e:
+            logging.error(f"Error in get_cpus_by_price: {str(e)}")
+            return e
+
+    # def read_cpu(self, brand: str, model: str):
+    #     for cpu in self._cpus:
+    #         if cpu.get_brand() == brand and cpu.get_model() == model:
+    #             return cpu
+    #     return None
 
 
 part_picker = PartPicker(cpus_list)
@@ -49,3 +75,38 @@ print("\nCPUs by price[lower or equal]\n")
 cpus_by_price = part_picker.get_cpus_by_price(250)
 for cpu in cpus_by_price:
     print(f"{cpu.get_brand()} {cpu.get_model()} - Price: {cpu.get_price()}€")
+
+print("Crud Operations: READ")
+found_cpu = read_cpu(part_picker._cpus, "AMD", "Ryzen 9 5950X")
+if found_cpu:
+    print("Found CPU:", found_cpu._model)
+else:
+    print("CPU not found.")
+
+print("\nCrud Operations: UPDATE")
+update_price = update_cpu(part_picker._cpus, "AMD", "Ryzen 9 5950X", 110.99)
+if update_price:
+    print("CPU price updated successfully.")
+else:
+    print("CPU not found, update failed.")
+
+print("\nCrud Operations: Check if price changed")
+cpu = read_cpu(part_picker._cpus, "AMD", "Ryzen 9 5950X")
+if cpu:
+    print("Updated CPU Price:", cpu.price)
+else:
+    print("CPU not found.")
+
+print("\nCrud Operations: DELETE")
+delete = delete_cpu(part_picker._cpus, "AMD", "Ryzen 9 5950X")
+if delete:
+    print("CPU deleted successfully.")
+else:
+    print("CPU not found, delete failed.")
+
+print("\nCrud Operations: Check deleted item.")
+check_deleted = read_cpu(part_picker._cpus, "AMD", "Ryzen 9 5950X")
+if check_deleted:
+    print("Found CPU:", found_cpu._model)
+else:
+    print("CPU not found.")
--- utilities/pc_parts.py
+++ utilities/pc_parts.py
@@ -49,29 +49,110 @@ class CPUColler(PCParts):
 
 
 class Motherboard(PCParts):
-    def __init__(self, brand: str, model: str, price: float, socket: str, form_factor: str, memory_max: int, memory_slots: int):
+    def __init__(self, brand: str, model: str, price: float, socket: str, form_factor: str, memory_max: int):
         super().__init__(brand, model, price)
         self.socket = socket
         self.form_factor = form_factor
         self.memory_max = memory_max
-        self.memory_slots = memory_slots
+
+    def get_socket(self):
+        return self.socket
+
+    def get_form_factor(self):
+        return self.form_factor
+
+    def get_memory_max(self):
+        return self.memory_max
 
 
 class Memory(PCParts):
-    pass
+    def __init__(self, brand: str, model: str, price: float, memory_quantity: int, memory_type: str, memory_speed: int):
+        super().__init__(brand, model, price)
+        self.memory_quantity = memory_quantity
+        self.memory_type = memory_type
+        self.memory_speed = memory_speed
+
+    def get_memory_quantity(self):
+        return self.memory_quantity
+
+    def get_memory_type(self):
+        return self.memory_type
+
+    def get_memory_speed(self):
+        return self.memory_speed
 
 
 class Storage(PCParts):
-    pass
+    def __init__(self, brand: str, model: str, price: float, capacity: str, type: str, interface: str):
+        super().__init__(brand, model, price)
+        self.capacity = capacity
+        self.type = type
+        self.interface = interface
+
+    def get_capacity(self):
+        return self.capacity
+
+    def get_type(self):
+        return self.type
+
+    def get_interface(self):
+        return self.interface
 
 
 class VideoCard(PCParts):
-    pass
+    def __init__(self, brand: str, model: str, price: float, chipset: str, memory_size: int, core_clock: int, boost_clock: int):
+        super().__init__(brand, model, price)
+        self.chipset = chipset
+        self.memory_size = memory_size
+        self.core_clock = core_clock
+        self.boost_clock = boost_clock
+
+    def get_chipset(self):
+        return self.chipset
+
+    def get_memory_size(self):
+        return self.memory_size
+
+    def get_core_clock(self):
+        return self.core_clock
+
+    def get_boost_clock(self):
+        return self.boost_clock
 
 
 class Case(PCParts):
-    pass
+    def __init__(self, brand: str, model: str, price: float, case_type: str, case_color: str, side_panel: str):
+        super().__init__(brand, model, price)
+        self.case_type = case_type
+        self.case_color = case_color
+        self.side_panel = side_panel
+
+    def get_case_type(self):
+        return self.case_type
+
+    def get_case_color(self):
+        return self.case_color
+
+    def get_side_panel(self):
+        return self.side_panel
 
 
 class PowerSupply(PCParts):
-    pass
+    def __init__(self, brand: str, model: str, price: float, power_supply_type: str, efficiency_rating: str, wattage: int, modular: str):
+        super().__init__(brand, model, price)
+        self.power_supply_type = power_supply_type
+        self.efficiency_rating = efficiency_rating
+        self.wattage = wattage
+        self.modular = modular
+
+    def get_power_supply_type(self):
+        return self.power_supply_type
+
+    def get_efficiency_rating(self):
+        return self.efficiency_rating
+
+    def get_wattage(self):
+        return self.wattage
+
+    def get_modular(self):
+        return self.modular
